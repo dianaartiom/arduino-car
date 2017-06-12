231f73f4c76c6458200421192bc1d7d804d7e880
Vitiok <lucavictor220@gmail.com>
2017-06-12 11:12:42 +0200

    Added simple server motor control which rotates 180 degrees

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "Arduino.h"
+#include "Servo.h"
+#include "setup.h"
+
+Servo servoMotor;
 
 void setup() {
+  servoMotor.attach(SERVO_MOTOR_PWM_PIN);
+  servoMotor.write(180);
   Serial.begin(9600);
 }
 
---
+++ src/setup.h
@@ -0,0 +1,4 @@
+#define PWM_PIN 6 // PWM Motor driver (/D2 pin pin on MC338870 motor board)
+#define MOTOR_PIN_1 7 // Motor pin 1 (IN1 on MC338870 motor board)
+#define MOTOR_PIN_2  8 // Motor pin 2 (IN2 on MC338870 motor board)
+#define SERVO_MOTOR_PWM_PIN 9
