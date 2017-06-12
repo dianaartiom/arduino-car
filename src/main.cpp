b81f8c7d1152e32dffd7601ed5975779a121040a
Vitiok <lucavictor220@gmail.com>
2017-06-12 20:20:15 +0200

    Added bluetooth simple example. On/OFF of the led

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 #include "setup.h"
 
 Servo servoMotor;
+char data = 0;
 
 void setup() {
   servoMotor.attach(SERVO_MOTOR_PWM_PIN);
   servoMotor.write(180);
   pinMode(MOTOR_PIN_1, OUTPUT); //Initiates Motor Channel A pin
   pinMode(MOTOR_PIN_2, OUTPUT); //Initiates Brake Channel A pin
+  pinMode(LED_TMP, OUTPUT); // Temporal led for testing bluetooth
   Serial.begin(9600);
 }
 
@@ -16,6 +18,23 @@ void loop() {
   digitalWrite(MOTOR_PIN_1, HIGH); //Establishes forward direction of Channel A
   digitalWrite(MOTOR_PIN_2, LOW);   //Disengage the Brake for Channel A
   analogWrite(PWM_PIN, 50);   //Spins the motor on Channel A at full speed
+
+  // Temp code for checking bluetooth
+  // Start ===========================
+  
+  if(Serial.available() > 0)      // Send data only when you receive data:
+  {
+     data = Serial.read();        //Read the incoming data & store into data
+     Serial.print(data);          //Print Value inside data in Serial monitor
+     Serial.print("\n");
+     if(data == '1')              // Checks whether value of data is equal to 1
+        digitalWrite(13, HIGH);   //If value is 1 then LED turns ON
+     else if(data == '0')         //  Checks whether value of data is equal to 0
+        digitalWrite(13, LOW);    //If value is 0 then LED turns OFF
+  }
+
+  // End =============================
+
   delay(300);
   Serial.println("Yeee it is working...");
 }
--- src/setup.h
+++ src/setup.h
@@ -2,3 +2,6 @@
 #define MOTOR_PIN_1 7 // Motor pin 1 (IN1 on MC338870 motor board)
 #define MOTOR_PIN_2  8 // Motor pin 2 (IN2 on MC338870 motor board)
 #define SERVO_MOTOR_PWM_PIN 9
+#define BLUETOOTH_TX 1
+#define BLUETOOTH_RX 0
+#define LED_TMP 13
