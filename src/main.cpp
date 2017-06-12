b9aea069ddf5ca4d74aeb2b69793fc8b59121137
Vitiok <lucavictor220@gmail.com>
2017-06-12 11:24:32 +0200

    Added basic motor control

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,21 @@
 #include "setup.h"
 
 Servo servoMotor;
+int val = 0;
 
 void setup() {
   servoMotor.attach(SERVO_MOTOR_PWM_PIN);
   servoMotor.write(180);
+  pinMode(MOTOR_PIN_1, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(MOTOR_PIN_2, OUTPUT); //Initiates Brake Channel A pin
   Serial.begin(9600);
 }
 
 void loop() {
-  delay(500);
+  digitalWrite(MOTOR_PIN_1, HIGH); //Establishes forward direction of Channel A
+  digitalWrite(MOTOR_PIN_2, LOW);   //Disengage the Brake for Channel A
+  analogWrite(PWM_PIN, val);   //Spins the motor on Channel A at full speed
+  val++;
+  delay(300);
   Serial.println("Yeee it is working...");
 }
